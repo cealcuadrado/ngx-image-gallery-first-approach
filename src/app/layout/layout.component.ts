803029dd7bbf0fdd9a0141b79e85f8cda6ee5540
CeAlCuadrado <cealcuadrado@outlook.com>
2022-01-31 19:22:42 -0300

    Instalada dependencia ngx-image-gallery, funcional

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,15 +1,89 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+  NgxImageGalleryComponent,
+  GALLERY_CONF,
+  GALLERY_IMAGE,
+} from 'ngx-image-gallery';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent implements OnInit {
+  @ViewChild(NgxImageGalleryComponent)
+  ngxImageGallery: NgxImageGalleryComponent;
 
-  constructor() { }
+  conf: GALLERY_CONF = {
+    imageOffset: '0px',
+    showDeleteControl: false,
+    showImageTitle: false,
+  };
 
-  ngOnInit(): void {
+  images: GALLERY_IMAGE[] = [
+    {
+      url: 'https://via.placeholder.com/1000.png/09f/fff',
+      altText: 'woman-in-black-blazer-holding-blue-cup',
+      title: 'woman-in-black-blazer-holding-blue-cup',
+      thumbnailUrl: 'https://via.placeholder.com/1000.png/09f/fff',
+    },
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  // METHODS
+  // open gallery
+  openGallery(index: number = 0) {
+    this.ngxImageGallery.open(index);
   }
 
+  // close gallery
+  closeGallery() {
+    this.ngxImageGallery.close();
+  }
+
+  // set new active(visible) image in gallery
+  newImage(index: number = 0) {
+    this.ngxImageGallery.setActiveImage(index);
+  }
+
+  // next image in gallery
+  nextImage(index: number = 0) {
+    this.ngxImageGallery.next();
+  }
+
+  // prev image in gallery
+  prevImage(index: number = 0) {
+    this.ngxImageGallery.prev();
+  }
+
+  /**************************************************/
+
+  // EVENTS
+  // callback on gallery opened
+  galleryOpened(index) {
+    console.info('Gallery opened at index ', index);
+  }
+
+  // callback on gallery closed
+  galleryClosed() {
+    console.info('Gallery closed.');
+  }
+
+  // callback on gallery image clicked
+  galleryImageClicked(index) {
+    console.info('Gallery image clicked with index ', index);
+  }
+
+  // callback on gallery image changed
+  galleryImageChanged(index) {
+    console.info('Gallery image changed to index ', index);
+  }
+
+  // callback on user clicked delete button
+  deleteImage(index) {
+    console.info('Delete image at index ', index);
+  }
 }
